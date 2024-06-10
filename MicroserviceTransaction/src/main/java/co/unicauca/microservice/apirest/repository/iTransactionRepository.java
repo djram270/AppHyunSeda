cc0bc807ebf584c957eceefc084e2c4940a82f34
Oscar Tosne Perez <54742876+djram270@users.noreply.github.com>
2024-06-10 17:02:12 -0500

    Delete MicroserviceTransaction directory

--- MicroserviceTransaction/src/main/java/co/unicauca/microservice/apirest/repository/iTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package co.unicauca.microservice.apirest.repository;
-
-import co.unicauca.microservice.apirest.domain.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface iTransactionRepository extends JpaRepository<Transaction, Long> {
-}
