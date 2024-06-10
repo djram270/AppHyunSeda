ad6b1c026a621b8bbbc41b826c1c39e8bdc09bd2
Oscar Tosne Perez <54742876+djram270@users.noreply.github.com>
2024-06-10 17:01:29 -0500

    Delete Client directory

--- Client/apirest/target/classes/ import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO CLIENT (first_Name, last_Name, address, username, password, email) VALUES ('Oscar', 'Martinez', '123 Main St, Anytown, USA', 'oscar123', 'secretpassword', 'oscar@example.com');
-INSERT INTO CLIENT (first_Name, last_Name, address, username, password, email) VALUES ('Oscar1', 'Martinez1', '123 Main St, Anytown, USA', 'oscar123', 'secretpassword', 'oscar@example.com');
-INSERT INTO CLIENT (first_Name, last_Name, address, username, password, email) VALUES ('Oscar2', 'Martinez2', '123 Main St, Anytown, USA', 'oscar123', 'secretpassword', 'oscar@example.com');
