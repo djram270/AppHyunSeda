b929e1aab2b184108643231e0111ea221b5bb9ae
Oscar Tosne Perez <54742876+djram270@users.noreply.github.com>
2024-06-12 10:26:21 -0500

    Delete MicroserviceOrder directory

--- MicroserviceOrder/src/main/java/co/unicauca/microservice/apirest/repository/iItemRepository.java
+++
@@ -1,8 +0,0 @@
-package co.unicauca.microservice.apirest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.unicauca.microservice.apirest.domain.entity.Item;
-
-public interface iItemRepository extends JpaRepository<Item, Long> {
-}
